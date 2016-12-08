843c21b6438f0abba2e05fb0bf72072bcc49de26
Greg D. Hill <greghill@cs.stanford.edu>
2016-12-08 13:07:26 -0800

    trying basic test

--- travis_test.sh
+++ travis_test.sh
@@ -6,4 +6,6 @@ cp .id_rsa.pub ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
 chmod 644 ~/.ssh/id_rsa.pub
 cat .id_rsa.pub >> ~/.ssh/authorized_keys
-mm-delay 5 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-delay 5 test/run.pyping -c 5 $MAHIMAHI_BASE'
+test/setup.py default_tcp
+PANTHEON_DIR=$(pwd)
+mm-delay 5 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-loss uplink .1 mm-loss downlink .1 test/test.py default_tcp -r $USER@$MAHIMAHI_BASE:$PANTHEON_DIR'
