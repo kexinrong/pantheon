6ea1f6b0f692e3254ebb24780bd9c3b1b4ae51d8
Greg Hill <greghill@users.noreply.github.com>
2016-12-08 04:10:18 -0800

    Oops

--- travis_test.sh
+++ travis_test.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 #test/setup.py default_tcp
 mkdir -p ~/.ssh
-cp .id_rsa .id_rsa.pub ~/.ssh
+cp .id_rsa ~/.ssh/id_rsa
+cp .id_rsa.pub ~/.ssh/id_rsa.pub
 cat .id_rsa >> ~/.ssh/authorized_keys
 mm-delay 5 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-delay 5 test/run.pyping -c 5 $MAHIMAHI_BASE'
