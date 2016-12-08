ae9949fce5d190353fee772b9ca7771fa565b8ba
Greg D. Hill <greghill@cs.stanford.edu>
2016-12-08 03:50:48 -0800

    use dummy rsa keypair

--- travis_test.sh
+++ travis_test.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 #test/setup.py default_tcp
-./.travis_make_ssh_key.sh
+mkdir -p ~/.ssh
+cp .id_rsa .id_rsa.pub ~/.ssh
+cat .id_rsa >> ~/.ssh/authorized_keys
 mm-delay 5 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-delay 5 test/run.pyping -c 5 $MAHIMAHI_BASE'
