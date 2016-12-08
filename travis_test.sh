01eb3b604dbbd4cd8927491d4c096628429ccae4
Greg Hill <greghill@users.noreply.github.com>
2016-12-08 03:23:36 -0800

    try make key script again

--- travis_test.sh
+++ travis_test.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 #test/setup.py default_tcp
-cat ~/.ssh/id_rsa.pub
-mkdir -p ~/.ssh
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+./.travis_make_ssh_key.sh
 mm-delay 5 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-delay 5 test/run.pyping -c 5 $MAHIMAHI_BASE'
