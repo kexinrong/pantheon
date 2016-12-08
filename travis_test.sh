36d2718e02e93d6ad9c617023c538fc500b2ec98
Greg Hill <greghill@users.noreply.github.com>
2016-12-08 03:03:08 -0800

    add self to auth keys

--- travis_test.sh
+++ travis_test.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 #test/setup.py default_tcp
+cat .ssh/id_rsa.pub >> .ssh/authorized_keys
 mm-delay 5 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-delay 5 ping -c 5 $MAHIMAHI_BASE'
