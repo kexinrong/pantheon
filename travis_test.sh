781a3d7ce2d965130d794cc8caffcfe9532fa82b
Greg Hill <greghill@users.noreply.github.com>
2016-12-08 02:52:08 -0800

    ssh  StrictHostKeyChecking=no

--- travis_test.sh
+++ travis_test.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 #test/setup.py default_tcp
-mm-delay 5 sh -c 'ssh $USER@$MAHIMAHI_BASE exit; mm-delay 5 ping -c 5 $MAHIMAHI_BASE'
+mm-delay 5 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-delay 5 ping -c 5 $MAHIMAHI_BASE'
