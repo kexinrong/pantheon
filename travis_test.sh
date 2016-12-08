91b4941a2564be967a73caf4a2bc2f707e423b7c
Greg D. Hill <greghill@cs.stanford.edu>
2016-12-08 14:00:31 -0800

    run full test with loss

--- travis_test.sh
+++ travis_test.sh
@@ -1,13 +1,12 @@
 #! /bin/bash -x
 
 mkdir -p ~/.ssh
+
 cp .id_rsa ~/.ssh/id_rsa
 cp .id_rsa.pub ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
 chmod 644 ~/.ssh/id_rsa.pub
 cat .id_rsa.pub >> ~/.ssh/authorized_keys
-test/setup.py default_tcp
-PANTHEON_DIR=$(pwd)
-echo $PANTHEON_DIR
 
-mm-delay 5 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-loss uplink .1 mm-loss downlink .1 test/test.py default_tcp -r $USER@$MAHIMAHI_BASE:build/StanfordLPNG/pantheon'
+test/run.py --run-only setup
+mm-delay 200 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-loss uplink .1 mm-loss downlink .1 test/run.py --run-only test -r $USER@$MAHIMAHI_BASE:build/StanfordLPNG/pantheon'
