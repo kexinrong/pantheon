5589427b804dbcd33c0d6c5eaf5443cb92b0cabf
Greg D. Hill <greghill@cs.stanford.edu>
2016-12-08 12:51:16 -0800

    fix rsa key permissions

--- travis_test.sh
+++ travis_test.sh
@@ -3,5 +3,7 @@
 mkdir -p ~/.ssh
 cp .id_rsa ~/.ssh/id_rsa
 cp .id_rsa.pub ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa
+chmod 644 ~/.ssh/id_rsa.pub
 cat .id_rsa.pub >> ~/.ssh/authorized_keys
 mm-delay 5 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-delay 5 test/run.pyping -c 5 $MAHIMAHI_BASE'
