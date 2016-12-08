9a6c6f144bd4407f06912ca67eae77a9b4011875
Greg D. Hill <greghill@cs.stanford.edu>
2016-12-08 13:22:11 -0800

    try two things and print out more

--- travis_test.sh
+++ travis_test.sh
@@ -1,5 +1,5 @@
-#! /bin/bash
-#test/setup.py default_tcp
+#! /bin/bash -x
+
 mkdir -p ~/.ssh
 cp .id_rsa ~/.ssh/id_rsa
 cp .id_rsa.pub ~/.ssh/id_rsa.pub
@@ -8,4 +8,8 @@ chmod 644 ~/.ssh/id_rsa.pub
 cat .id_rsa.pub >> ~/.ssh/authorized_keys
 test/setup.py default_tcp
 PANTHEON_DIR=$(pwd)
+echo $PANTHEON_DIR
+
+mm-delay 5 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-loss uplink .1 mm-loss downlink .1 test/test.py default_tcp -r $USER@$MAHIMAHI_BASE:pantheon'
+
 mm-delay 5 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-loss uplink .1 mm-loss downlink .1 test/test.py default_tcp -r $USER@$MAHIMAHI_BASE:$PANTHEON_DIR'
