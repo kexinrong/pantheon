9531a596299772c1d8a298b9a7fd73f15afa975a
Greg D. Hill <greghill@cs.stanford.edu>
2016-12-08 17:05:52 -0800

    do some extra ntp from travis testing

--- travis_test.sh
+++ travis_test.sh
@@ -1,12 +1,15 @@
 #! /bin/bash -x
 
-mkdir -p ~/.ssh
-
 cp .id_rsa ~/.ssh/id_rsa
 cp .id_rsa.pub ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
 chmod 644 ~/.ssh/id_rsa.pub
 cat .id_rsa.pub >> ~/.ssh/authorized_keys
+
+ntpdate -quv us.pool.ntp.org
+ntpdate -quv time.stanford.edu
+mm-delay 10 ntpdate -quv time.stanford.edu
+mm-loss uplink .1 mm-loss downlink .1 ntpdate -quv time.stanford.edu
 
 test/run.py --run-only setup
 mm-delay 200 sh -c 'ssh -o "StrictHostKeyChecking=no" $USER@$MAHIMAHI_BASE exit; mm-loss uplink .1 mm-loss downlink .1 test/run.py --run-only test -r $USER@$MAHIMAHI_BASE:build/StanfordLPNG/pantheon'
